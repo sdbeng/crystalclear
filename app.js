08d3240bd5adcd5e7c9c734a08a646550347e8eb
Sergio Barrera <sdbeng@gmail.com>
2018-10-10 11:14:00 -0700

    test clicks

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-console.log('game starting')
+// console.log('game starting')
+
 // define global variables
 var currentScode = 0
 var targetScore = 0
 
 // create crystals object
 var crystals = {
-blue: { name: 'blue', value: 0 },
-green: { name: 'green', value: 0 },
-red: { name: 'red', value: 0 },
-yellow: { name: 'yellow', value: 0 }
+	blue: { name: "blue", value: 0 },
+	green: { name: "green", value: 0 },
+	red: { name: "red", value: 0 },
+	yellow: { name: "yellow", value: 0 }
 }
 // functions
 
 // main game process
+
+// test clicks
+$("#blue").click(() => {
+	console.log("clicked blue!!")
+
+})
+$("#green").click(() => {
+	console.log("clicked green!!")
+
+})
+$("#red").click(() => {
+	console.log("clicked red!!")
+
+})
+$("#yellow").click(() => {
+	console.log("clicked yellow!!")
+
+})
