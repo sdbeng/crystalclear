f50cf59fa2a3264621f1529bd4ade4023e9deb24
Sergio Barrera <sdbeng@gmail.com>
2018-10-10 11:39:43 -0700

    random values testing works

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // console.log('game starting')
 
 // define global variables
-var currentScode = 0
+var currentScore = 0
 var targetScore = 0
 
 // create crystals object
@@ -12,16 +12,48 @@ var crystals = {
 	yellow: { name: "yellow", value: 0 }
 }
 // functions
+let getRandom = (max, min) => {
+	let random = Math.floor(Math.random() * (max - min + 1)) + min
+	return random
+}
 
+let startGame = () => {
+    // reset currentScore 120 - 19
+    currentScore = 0
+    // set a new targetScore
+    targetScore = getRandom(120, 19)
+
+    // set diff values for each crystal - use obj notation
+    crystals.blue.value = getRandom(12, 1)
+    crystals.green.value = getRandom(12, 1)
+    crystals.red.value = getRandom(12, 1)
+    crystals.yellow.value = getRandom(12, 1)
+    // render values to DOM
+    // test values first
+    console.log("current Score: ", currentScore);
+    console.log("target Score", targetScore);
+    console.log("blue ", crystals.blue.value);
+    console.log("green ", crystals.green.value);
+    console.log("red ", crystals.red.value);
+    console.log("yellow ", crystals.yellow.value);
+    
+    
+    
+}
 // main game process
-
+startGame()
 // test clicks
-$("#blue").click(() => {
+$("#blue").click((crystal) => {
 	console.log("clicked blue!!")
+    // get a random value when i click on a crystal
+    
 
 })
-$("#green").click(() => {
-	console.log("clicked green!!")
+$("#green").click((crystal) => {
+    console.log("clicked green!!")
+    
+    // get a random value for the passed crystal
+    
 
 })
 $("#red").click(() => {
