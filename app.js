5d993468c27170be12f08b4cacf423ae6e6a6de0
Sergio Barrera <sdbeng@gmail.com>
2018-10-10 12:03:31 -0700

    logging rand crystal values

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // console.log('game starting')
 
+// hint: in order to avoid value flickering, set the div to an empty div in the index html
+
 // define global variables
 var currentScore = 0
 var targetScore = 0
@@ -17,43 +19,59 @@ let getRandom = (max, min) => {
 	return random
 }
 
-let startGame = () => {
-    // reset currentScore 120 - 19
-    currentScore = 0
-    // set a new targetScore
-    targetScore = getRandom(120, 19)
+const startGame = () => {
+	// reset currentScore 120 - 19
+	currentScore = 0
+	// set a new targetScore
+	targetScore = getRandom(120, 19)
 
-    // set diff values for each crystal - use obj notation
-    crystals.blue.value = getRandom(12, 1)
-    crystals.green.value = getRandom(12, 1)
-    crystals.red.value = getRandom(12, 1)
-    crystals.yellow.value = getRandom(12, 1)
-    // render values to DOM
-    // test values first
-    console.log("current Score: ", currentScore);
-    console.log("target Score", targetScore);
-    console.log("blue ", crystals.blue.value);
-    console.log("green ", crystals.green.value);
-    console.log("red ", crystals.red.value);
-    console.log("yellow ", crystals.yellow.value);
+	// set diff values for each crystal - use obj notation
+	crystals.blue.value = getRandom(12, 1)
+	crystals.green.value = getRandom(12, 1)
+	crystals.red.value = getRandom(12, 1)
+	crystals.yellow.value = getRandom(12, 1)
+
+	// render values to DOM
+	$("#targetScore").html(targetScore)
+	$("#currentScore").html(currentScore)
+
+	// experience declaring a local const in ethis scope to avoid typing many console logs
+	const newLocal = console.log
+	// test values first
+	newLocal("current Score: ", currentScore)
+	newLocal("target Score", targetScore)
+	newLocal("blue ", crystals.blue.value)
+	newLocal("green ", crystals.green.value)
+	newLocal("red ", crystals.red.value)
+	newLocal("yellow ", crystals.yellow.value)
+
+}
+// create func to add values when crystals are clicked
+// instead of passing a value, i'm passing a crystal obj
+const addValues = (crystal) => {
+	// update currentScore
+	currentScore += crystal.value
     
-    
+	// test current score
+	console.log("Your score", currentScore)
     
 }
+
 // main game process
 startGame()
 // test clicks
-$("#blue").click((crystal) => {
+$("#blue").click(() => {
 	console.log("clicked blue!!")
-    // get a random value when i click on a crystal
-    
+	// call func passing each obj
+	addValues(crystals.blue)
+
 
 })
 $("#green").click((crystal) => {
-    console.log("clicked green!!")
-    
-    // get a random value for the passed crystal
-    
+	console.log("clicked green!!")
+
+	// get a random value for the passed crystal
+
 
 })
 $("#red").click(() => {
