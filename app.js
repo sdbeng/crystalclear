9cb9c33d6309ede7b2f7f7761aa81d148887045d
Sergio Barrera <sdbeng@gmail.com>
2018-10-10 11:01:34 -0700

    basic layout

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('game starting')
+// define global variables
+var currentScode = 0
+var targetScore = 0
+
+// create crystals object
+var crystals = {
+blue: { name: 'blue', value: 0 },
+green: { name: 'green', value: 0 },
+red: { name: 'red', value: 0 },
+yellow: { name: 'yellow', value: 0 }
+}
+// functions
+
+// main game process
